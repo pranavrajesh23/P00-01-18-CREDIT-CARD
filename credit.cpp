455edd8a9ccec1d1b9ebd97cb2c8f3dfc8f5ca90
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 20:58:10 +0530

    verification fucntion

--- credit.cpp
+++ credit.cpp
@@ -4,6 +4,60 @@ using namespace std;
 string name,order;
 int amt;
 
+bool validCard()
+{
+    string valid;
+    cout<<"\nIs it a valid card? (yes/no)";
+    cin>>valid;
+    return valid=="yes";
+}
+
+bool fundAvailable()
+{
+    string fund;
+    cout<<"\nFund avaiable? (yes/no)";
+    cin>>fund;
+    return fund=="yes";
+}
+
+bool validAcnt()
+{
+    string acnt;
+    cout<<"\nIs it a valid account? (yes/no)";
+    cin>>acnt;
+    return acnt=="yes";
+}
+
+void verification()
+{
+    cout<<"\n----Verification Process----\n";
+    while(true)
+    {
+        if(validCard())
+        {
+            if(fundAvailable())
+            {
+                if(validAcnt())
+                {
+                    cout<<"\nPayment verified by the bank";
+                    break;
+                }
+                else{
+                    cout<<"\nNot a valid account\n";
+                    continue;
+                }
+            }
+            {
+                cout<<"\nFunds not available, try again\n";
+            }
+        }
+        else
+        {
+            cout<<"\nNot a valid card, try again\n";
+        }
+    }
+}
+
 int main()
 {
     cout<<"\n-----Credit Card Payment----\n";
